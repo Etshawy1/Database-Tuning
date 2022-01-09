de3a2211871c3a8ed1889ea6a7cf0a16f4fb51e4
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2022-01-09 22:36:14 +0200

    Fix: example of query optimization

--- queries/query_optimization.sql
+++ queries/query_optimization.sql
@@ -20,16 +20,14 @@ SET @@profiling_history_size = 100;
 SET @@profiling = 1;
 
 use optimized_university_hundred_thousand;
-create index index_student_name using HASH  ON student(Name);
-create index index_city using HASH  ON student(City);
 
 select
 	distinct s.ID, s.Name, s.City
 from
 (
-(select s2.ID, s2.Name, s2.City from student s2 use index(index_student_name) where s2.Name = "Demond Valentine")
+(select s2.ID, s2.Name, s2.City from student s2 where s2.Name = "Demond Valentine")
 union 
-(select s3.ID, s3.Name, s3.City from student s3 use index(index_city) where s3.City = "Miami")
+(select s3.ID, s3.Name, s3.City from student s3 where s3.City = "Miami")
 ) as s;
 show profiles;
 
@@ -56,13 +54,8 @@ use optimized_university_hundred_thousand;
 select
 	distinct s.ID, s.Name, s.City
 from
-	student s use index(index_student_name, index_city)
+	student s
 where
 	(s.Name = "Demond Valentine"
 	or s.City = "Miami");
 show profiles;
-
-
-
-DROP INDEX index_student_name ON student;
-DROP INDEX index_city ON student;
